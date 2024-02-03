e25326ad2c2bdf19d987c5ff46214f6fd8cadb9c
L0giXX <marc.mueller04@gmail.com>
2024-02-03 23:12:20 +0100

    created custom card component, finished first landing page prototype

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { CardComponent } from './components/ui/card/card.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MatCardModule, MatIcon, CardComponent],
+  template: `
+    <div class="div-main">
+      <header>
+        <nav>
+          <a id="nav-a-main" href="#">HR GUI</a>
+          <a href="#"> Dashboard </a>
+          <a href="#"> Employees </a>
+          <a href="#"> Departments </a>
+          <a href="#"> Reports </a>
+        </nav>
+      </header>
+      <main>
+        <h1>Welcome to Acme Inc HR System</h1>
+        <p>Manage your employees and departments with ease.</p>
+        <section>
+          <app-card [cardTitle]="'Employee count'" [cardValue]="235"></app-card>
+          <app-card
+            [cardTitle]="'Department count'"
+            [cardValue]="12"
+          ></app-card>
+          <app-card
+            [cardTitle]="'Average salary'"
+            [cardValue]="65000"
+          ></app-card>
+        </section>
+      </main>
+      <footer>
+        <p>© 2024 Acme Inc. All rights reserved.</p>
+        <nav><a href="#"> Terms of Service </a><a href="#"> Privacy </a></nav>
+      </footer>
+    </div>
+  `,
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'hr-gui';
+}
